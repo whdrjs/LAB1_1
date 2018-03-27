89195441eaf5ad167190a61b6cb3e8a4c67b0f39
Jongkeon <30586378+whdrjs@users.noreply.github.com>
2018-03-27 09:39:04 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/lab1_1/MainActivity.java
+++ app/src/main/java/com/example/lab1_1/MainActivity.java
@@ -8,28 +8,31 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     ImageView imageView2;
-    int imgBoolean=0;
+    int imgBoolean=0;       //check image has changed
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //match android widget
         imageView=(ImageView)findViewById(R.id.imageView1);
         imageView2= (ImageView) findViewById(R.id.imageView2);
     }
+    //when button(id:button) is clicked, operation
     public void onButtonClicked(View v){
         changeImage();
     }
+    //changed image
     private void changeImage(){
         if(imgBoolean == 0){
             imgBoolean = 1;
-            imageView.setVisibility(View.VISIBLE);
-            imageView2.setVisibility(View.INVISIBLE);
+            imageView.setVisibility(View.INVISIBLE);    //hide imageView1
+            imageView2.setVisibility(View.VISIBLE);     //show imageView2
         }
         else if (imgBoolean== 1){
             imgBoolean = 0;
-            imageView.setVisibility(View.INVISIBLE);
-            imageView2.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.VISIBLE);      //show imageView1
+            imageView2.setVisibility(View.INVISIBLE);   //hide imageView2
 
         }
     }
